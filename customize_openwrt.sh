5e94c12827380e1b9c717316baa804478857401a
Architect111 <85819354+Architect111@users.noreply.github.com>
2026-07-08 08:33:38 +0800

    Create customize_openwrt.sh

---
+++ customize_openwrt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export WGET_OPTS="--timeout=60 --tries=5"
+GH_MIRROR="${USTC_GITHUB_MIRROR}"
+OPENWRT_MIRROR="${USTC_OPENWRT}"
+
+# 清空原有feeds
+> feeds.conf.default
+# OpenWrt官方源
+echo "src-git packages ${GH_MIRROR}/openwrt/packages.git;openwrt-24.10" >> feeds.conf.default
+echo "src-git luci ${GH_MIRROR}/openwrt/luci.git;openwrt-24.10" >> feeds.conf.default
+echo "src-git routing ${GH_MIRROR}/openwrt/routing.git;openwrt-24.10" >> feeds.conf.default
+# 你的第三方插件库
+echo "src-git small ${GH_MIRROR}/Architect111/openwrt-packages.git" >> feeds.conf.default
+echo "src-git smallpkg ${GH_MIRROR}/Architect111/small-package.git" >> feeds.conf.default
+echo "src-git mypkg ${GH_MIRROR}/Architect111/my_personal_packages.git" >> feeds.conf.default
+
+# 替换下载镜像
+sed -i "s|https://downloads.openwrt.org|${OPENWRT_MIRROR}|g" feeds.conf.default
+
+# 更新安装feeds
+./scripts/feeds clean
+./scripts/feeds update -a
+./scripts/feeds install -a
